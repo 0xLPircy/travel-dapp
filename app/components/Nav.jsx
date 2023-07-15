75bfdb09637eb292d37229fd69d0466f1090d690
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-15 14:38:04 +0530

    clerk provider

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,7 +1,22 @@
+import Link from "next/link"
 
 const Nav = () => {
     return (
-        <div>Nav</div>
+        <>
+            <nav>
+                <Link href="/">
+                    <div id="logo">
+                        <h1>TRAVEL LOGO</h1>
+                    </div>
+                </Link>
+                <div>
+                    <Link>Home</Link>
+                    <Link>About</Link>
+                    <Link>Programs</Link>
+                    <Link>Contact Us</Link>
+                </div>
+            </nav>
+        </>
     )
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,6 @@ import Nav from "./components/Nav";
 export default function Home() {
   return (
     <>
-      <h1>TESTINGF</h1>
       <Nav />
     </>
   )
