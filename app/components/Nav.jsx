6291690c20eb2f637495602ca03e71fc5cd1202a
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-16 16:16:19 +0530

    Proposal OPDF Added

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link"
 import { auth, UserButton } from "@clerk/nextjs"
+import Image from "next/image";
 
 const Nav = () => {
     const { userId } = auth();
@@ -7,30 +8,31 @@ const Nav = () => {
 
     return (
         <>
-            <nav>
-                <Link href="/">
-                    <div id="logo">
-                        <h1>TRAVEL LOGO</h1>
+            <nav className="py-4 px-6 flex items-center justify-between mb-5">
+                <Link className="flex items-center" href="/">
+                    <div id="logo ">
+                        <h1 className="text-lg uppercase font-bold text-black">TRAVEL LOGO</h1>
                     </div>
                 </Link>
-                <div>
-                    {/* <Link>Home</Link>
-                    <Link>About</Link>
-                    <Link>Programs</Link>
-                    <Link>Contact Us</Link> */}
-                </div>
-                {!userId && (
-                    <>
-                        <Link href="sign-in"> Connect Wallet </Link>
-                    </>
-                )}
-                {userId && (
-                    <Link href="profile">
-                        Dashboard
-                    </Link>
-                )}
-                <div className="ml-auto">
-                    <UserButton afterSignOutUrl='/' />
+                <div className=" flex items-center">
+                    <Link className="mx-6" href="/">Home</Link>
+                    <Link className="mx-6" href="/">About</Link>
+                    <Link className="mx-6" href="/">Programs</Link>
+                    <Link className="mx-6" href="/">Contact Us</Link>
+                    {!userId && (
+                        <>
+                            <Link className="mx-6" href="sign-in"> Connect Wallet </Link>
+                            <Image></Image>
+                        </>
+                    )}
+                    {userId && (
+                        <Link className="mx-6" href="profile">
+                            Dashboard
+                        </Link>
+                    )}
+                    <div className="ml-auto">
+                        <UserButton afterSignOutUrl='/' />
+                    </div>
                 </div>
             </nav>
         </>
