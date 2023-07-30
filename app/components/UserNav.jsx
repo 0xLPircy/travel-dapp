2f807a8087d1dacabd037b554bd0546563f576d2
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-30 22:17:19 +0530

    dashboard content

--- app/components/UserNav.jsx
+++ app/components/UserNav.jsx
@@ -4,18 +4,18 @@ import logo from "@/assets/userNavLogo.png";
 
 const UserNav = () => {
   return (
-    <div>
+    <div className="">
       <div>
-        <Image height={60} width={60} src={logo} />
+        <Image height={180} width={240} src={logo} />
       </div>
       <div>
-        <ul>
+        <ul className="-mt-9 grid grid-flow-row items-start gap-3 text-[#b8b8b8] px-3 font-[300]">
           <li>Home</li>
           <li>Explore Trips</li>
           <li>DashBoard</li>
           <li>Past Trips</li>
           <li>My Rewards</li>
-          <li></li>
+          <li className="bg-[#b0b0b0] border-[#b0b0b0] border my-3"></li>
           <li>Settings</li>
           <li>Help & FAQ</li>
         </ul>
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -8,7 +8,50 @@ const DashboardPage = () => {
         {" "}
         <UserNav />
       </div>
-      <div className="col-span-3">center</div>
+      <div className="col-span-3">
+        <div>
+          <h1>Hi! Welcome to your Dashboard!</h1>
+          <h1>
+            Youre Currently on <span>Trip name</span>
+          </h1>
+        </div>
+        <div>
+          <h1>Tasks</h1>
+          <div>
+            <div>
+              <h2>Task Name</h2>
+              <div>
+                <h3>Difficulty of Task</h3>
+                {/* etherium stars */}
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+                qui odit dolor corporis, itaque, iure facilis molestiae laborum
+                vero provident veritatis non magnam autem iusto, culpa minus
+                perspiciatis hic dignissimos.
+              </p>
+              <div>
+                <button>View Reward</button>
+                <button>Submit Proof</button>
+              </div>
+            </div>
+            <div>
+              <div>
+                {/* navigation buttons */}
+                {/* Image */}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            {" "}
+            <h1>Colleted NFTs</h1>
+            {/* maybe sorting */}
+          </div>
+          <div>{/* nft card */}</div>
+        </div>
+      </div>
       <div className="col-span-1 bg-slate-400">rhs</div>
     </div>
   );
